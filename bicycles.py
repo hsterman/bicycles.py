b83d88c8d55b7446212ced5cb62009cecef8c3ae
hsterman <heathersterman@hotmail.com>
2015-03-10 00:14:09 +0000

    first commit

---
+++ bicycles.py
@@ -0,0 +1,30 @@
+class ClassName:
+  "Optional class documentation string"
+   class_suite
+    
+class Bicycle:
+  "Common base class for all Bicycles"
+  empCount = 0
+  
+   
+def __init__(model, weight, cost):
+      model.name = Hercules
+      model.weight = 44
+      model.cost = 200
+      Bicycle.empCount +=1
+      
+  class Shop:
+  "Common base class for all Shops"
+  empCount = 0    
+      
+def __init__(shop, inventory):
+      shop.name = Speedy Bikes
+      model.invetory = 60
+      Bicycle.empCount +=1
+      
+      
+      
+      
+  
+  
+  

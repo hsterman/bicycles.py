b6b4bed70f65f1695c57f740a2df52409f0c77ee
hsterman <hsterman@users.noreply.github.com>
2015-03-15 22:29:02 -0400

    Update bicycles.py

--- bicycles.py
+++ bicycles.py
@@ -4,23 +4,33 @@ class ClassName:
     
 class Bicycle:
   "Common base class for all Bicycles"
-  empCount = 0
-  
-   
-def __init__(model, weight, cost):
-      model.name = Hercules
-      model.weight = 44
-      model.cost = 200
+  empCount = 0 
+  def __init__(self, name, weight, cost):
+      self.name = name 
+      self.weight = weight
+      self.cost = cost
       Bicycle.empCount +=1
       
-  class Shop:
+      
+class Shop:
   "Common base class for all Shops"
   empCount = 0    
       
-def __init__(shop, inventory):
-      shop.name = Speedy Bikes
-      model.invetory = 60
-      Bicycle.empCount +=1
+  def __init__(self, name, inventory, cost, profit):
+      self.name = name
+      self.invetory = inventory
+      self.cost = cost
+      self.profit = profit
+      Shop.empCount +=1
+      
+class Customers:
+  "Common base class for all Customers"
+  empCount = 0
+  def __init__(self, name, fund, bicycle):
+      self.name = name
+      self.fund = fund
+      self.bicycle = bicycle
+      Shop.empCount +=1
       
       
       

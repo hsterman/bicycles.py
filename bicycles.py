cee6512a650e9d805d33d6c76be4c1f86e0dfa19
hsterman <hsterman@users.noreply.github.com>
2015-03-20 17:24:28 -0400

    Update bicycles.py

--- bicycles.py
+++ bicycles.py
@@ -1,6 +1,6 @@
 class ClassName:
   "Optional class documentation string"
-   class_suite
+
     
 class Bicycle:
   "Common base class for all Bicycles"
@@ -10,6 +10,7 @@ class Bicycle:
       self.weight = weight
       self.cost = cost
       Bicycle.empCount +=1
+      
       
       
 class Shop:
@@ -31,6 +32,26 @@ class Customers:
       self.fund = fund
       self.bicycle = bicycle
       Shop.empCount +=1
+      
+      
+      
+      
+  
+  
+
+bicycle1 = Bicycle("two wheel","30", "100")
+bicycle2 = Bicycle("tricycle", "20", "40")
+bicycle3 = Bicycle("unicycle", "15", "20")
+bicycle4 = Bicycle("moped", "35", "100")
+bicycle5 = Bicycle("scooter", "35", "90")
+bicycle6 = Bicycle("ten speed", "25", "70")
+
+
+
+customer1 = Customer("Bill","$200")
+customer2 = Customer("Betty", "$500")
+customer3 = Customer("Bob", "$1000")
+    
       
       
       
